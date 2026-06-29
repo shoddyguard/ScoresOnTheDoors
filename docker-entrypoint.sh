b4744dec20270b1a09098fb2dc026d58dbcf8d75
Steve Brown <steve.brown+github@brownserve.co.uk>
2026-06-29 09:12:39 +0100

    feat: allow setting  PUID/PGID for the container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 set -e
 
+# Allow the container to run as an arbitrary UID/GID.
+# Set PUID and/or PGID at runtime to override the defaults (both 1001).
+PUID=${PUID:-1001}
+PGID=${PGID:-1001}
+
+CURRENT_UID=$(id -u nextjs)
+CURRENT_GID=$(getent group nodejs | cut -d: -f3)
+
+if [ "$PGID" != "$CURRENT_GID" ]; then
+    groupmod -g "$PGID" nodejs
+fi
+
+if [ "$PUID" != "$CURRENT_UID" ]; then
+    usermod -u "$PUID" nextjs
+fi
+
+chown nextjs:nodejs /data
+
 echo "Running database migrations..."
-npx prisma db push --skip-generate
+gosu nextjs npx prisma db push --skip-generate
 
 echo "Starting application..."
-exec node server.js
+exec gosu nextjs node server.js
